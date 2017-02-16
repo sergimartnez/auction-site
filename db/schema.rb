11ebe1e5b83d47b0e654c7ecb9ea5f8e2144d077
Sergio Martinez <sergimartnez@yahoo.es>
2017-02-16 20:26:11 +0100

    changes to modify db to pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,14 +12,17 @@
 
 ActiveRecord::Schema.define(version: 20170121132146) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "bids", force: :cascade do |t|
     t.integer  "product_id"
     t.integer  "user_id"
     t.integer  "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["product_id"], name: "index_bids_on_product_id"
-    t.index ["user_id"], name: "index_bids_on_user_id"
+    t.index ["product_id"], name: "index_bids_on_product_id", using: :btree
+    t.index ["user_id"], name: "index_bids_on_user_id", using: :btree
   end
 
   create_table "products", force: :cascade do |t|
@@ -30,7 +33,7 @@ ActiveRecord::Schema.define(version: 20170121132146) do
     t.datetime "deadline"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.index ["user_id"], name: "index_products_on_user_id"
+    t.index ["user_id"], name: "index_products_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
